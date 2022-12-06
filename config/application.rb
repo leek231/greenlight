0529efeca9bf2b167b4541dc6573fa282fd82046
leek <vovanhikkan@gmail.com>
2022-12-06 19:19:08 +0500

    fixes

--- config/application.rb
+++ config/application.rb
@@ -145,7 +145,7 @@ module Greenlight
     # DEFAULTS
 
     # Default branding image if the user does not specify one
-    config.branding_image_default = "https://raw.githubusercontent.com/bigbluebutton/greenlight/master/app/assets/images/logo_with_text.png"
+    config.branding_image_default = "https://ia.wampi.ru/2022/04/04/LOGO-VZNANIY4.png"
 
     # Default primary color if the user does not specify one
     config.primary_color_default = "#467fcf"
